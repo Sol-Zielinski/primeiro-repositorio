00f851b61b1331110175ecc6cbc12d48ca4459aa
Sol-Zielinski <isis.zielinski@escola.pr.gov.br>
2026-08-10 21:56:36 -0300

    .

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ botao.addEventListener("click", botaoClicado);
 function botaoClicado () {
     console.log("fui clicado");
     let texto = botao.querySelector("span");
-    if(curtiu === false) {
+    if (curtiu === false) {
         texto.textContent++;
         curtiu = true;
     } else {
         texto.textContent--;
-        curtiu = fase;
+        curtiu = false;
     }
     }
 })
